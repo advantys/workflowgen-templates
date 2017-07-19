36f717502f2304e767f1558988fd5efc86a61047
maylinmedina <maylin.medina@advantys.com>
2017-07-19 12:08:13 -0400

    Update integration\webhooks\sendhttppost\... folder names

---
+++ integration/webhooks/sendhttppost/src/DataTypes/WebhookTest.Api/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebhookTest.Api.Models
+{
+    public class Response
+    {
+        public string Date_Utc_Now { get; set; }
+        public int Two_Squared_Number { get; set; }
+        public string Hello_Name { get; set; }
+    }
+}
---
+++ integration/webhooks/sendhttppost/src/Objects/WebhookTest.Api/Models/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebhookTest.Api.Models
+{
+    public class Response
+    {
+        public Person Person { get; set; }
+
+    }
+}
