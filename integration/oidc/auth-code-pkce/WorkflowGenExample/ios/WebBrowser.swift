7eb47d53d3161fffe786cc365be3a2ae8460a3dd
Charles Levesque <charles.levesque@advantys.com>
2019-05-21 14:58:45 -0400

    refactor: Re-indented swift code with 4 spaces

--- integration/oidc/auth-code-pkce/WorkflowGenExample/ios/WebBrowser.swift
+++ integration/oidc/auth-code-pkce/WorkflowGenExample/ios/WebBrowser.swift
@@ -3,65 +3,65 @@ import SafariServices
 
 @objc(WebBrowser)
 class WebBrowser : RCTEventEmitter, SFSafariViewControllerDelegate {
-  private static let SafariViewOnShow = "SafariViewOnShow"
-  private static let SafariViewOnDismiss = "SafariViewOnDismiss"
-
-  private var hasListeners = false
-  private var safariViewController: SFSafariViewController?
-
-  override func startObserving() {
-    self.hasListeners = true
-  }
-
-  override func stopObserving() {
-    self.hasListeners = false
-  }
-
-  override var methodQueue: DispatchQueue! { return DispatchQueue.main }
-
-  override static func requiresMainQueueSetup() -> Bool { return true }
-
-  override func constantsToExport() -> [AnyHashable : Any]! {
-    return [
-      "SafariViewOnShow": WebBrowser.SafariViewOnShow,
-      "SafariViewOnDismiss": WebBrowser.SafariViewOnDismiss
-    ]
-  }
-
-  override func supportedEvents() -> [String]! {
-    return [ WebBrowser.SafariViewOnShow, WebBrowser.SafariViewOnDismiss ]
-  }
-
-  @objc(show:resolve:reject:)
-  func show(_ args: NSDictionary!, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    guard let url = RCTConvert.nsurl(args["url"]) else {
-      reject("E_WEB_BROWSER_NO_URL", "You must specify a url to show.", nil)
-      return
+    private static let SafariViewOnShow = "SafariViewOnShow"
+    private static let SafariViewOnDismiss = "SafariViewOnDismiss"
+    
+    private var hasListeners = false
+    private var safariViewController: SFSafariViewController?
+    
+    override func startObserving() {
+        self.hasListeners = true
     }
-
-    self.safariViewController = SFSafariViewController(url: url)
-    self.safariViewController?.delegate = self
-    self.safariViewController?.modalPresentationStyle = .overFullScreen
-
-    RCTPresentedViewController()?.present(self.safariViewController!, animated: true, completion: nil)
-
-    if (self.hasListeners) {
-      self.sendEvent(withName: WebBrowser.SafariViewOnShow, body: nil)
+    
+    override func stopObserving() {
+        self.hasListeners = false
     }
-
-    resolve(true)
-  }
-
-  @objc(dismiss)
-  func dismiss() {
-    self.safariViewController?.dismiss(animated: true, completion: nil)
-  }
-
-  func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-    self.safariViewController = nil
-
-    if (self.hasListeners) {
-      self.sendEvent(withName: WebBrowser.SafariViewOnDismiss, body: nil)
+    
+    override var methodQueue: DispatchQueue! { return DispatchQueue.main }
+    
+    override static func requiresMainQueueSetup() -> Bool { return true }
+    
+    override func constantsToExport() -> [AnyHashable : Any]! {
+        return [
+            "SafariViewOnShow": WebBrowser.SafariViewOnShow,
+            "SafariViewOnDismiss": WebBrowser.SafariViewOnDismiss
+        ]
     }
-  }
+    
+    override func supportedEvents() -> [String]! {
+        return [ WebBrowser.SafariViewOnShow, WebBrowser.SafariViewOnDismiss ]
+    }
+    
+    @objc(show:resolve:reject:)
+    func show(_ args: NSDictionary!, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+        guard let url = RCTConvert.nsurl(args["url"]) else {
+            reject("E_WEB_BROWSER_NO_URL", "You must specify a url to show.", nil)
+            return
+        }
+        
+        self.safariViewController = SFSafariViewController(url: url)
+        self.safariViewController?.delegate = self
+        self.safariViewController?.modalPresentationStyle = .overFullScreen
+        
+        RCTPresentedViewController()?.present(self.safariViewController!, animated: true, completion: nil)
+        
+        if (self.hasListeners) {
+            self.sendEvent(withName: WebBrowser.SafariViewOnShow, body: nil)
+        }
+        
+        resolve(true)
+    }
+    
+    @objc(dismiss)
+    func dismiss() {
+        self.safariViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        self.safariViewController = nil
+        
+        if (self.hasListeners) {
+            self.sendEvent(withName: WebBrowser.SafariViewOnDismiss, body: nil)
+        }
+    }
 }
--- integration/oidc/auth-code-pkce/WorkflowGenExample/ios/WorkflowGenExample/AppDelegate.m
+++ integration/oidc/auth-code-pkce/WorkflowGenExample/ios/WorkflowGenExample/AppDelegate.m
@@ -16,33 +16,33 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"WorkflowGenExample"
-                                            initialProperties:nil];
-
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  return YES;
+    RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                     moduleName:@"WorkflowGenExample"
+                                              initialProperties:nil];
+    
+    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UIViewController *rootViewController = [UIViewController new];
+    rootViewController.view = rootView;
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
+    return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+    return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
 {
-  return [RCTLinkingManager application:app openURL:url options:options];
+    return [RCTLinkingManager application:app openURL:url options:options];
 }
 
 @end
